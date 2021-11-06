74b89409e5a9dc1c43fa2ff9a4058ad63e4e1503
DimonD <dprohorchuk2002@gmail.com>
2021-11-06 16:04:22 +0200

    Added general logic

---
+++ XMLParser/IXMLFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace XMLParser {
+
+	public struct Scientist {
+		string fullName;
+		string faculty;
+		string department;
+		string scientificDegree;
+		Dictionary<string, string> academicTitles;
+	}
+	
+	public interface IXMLFinder {
+		List<Scientist> Find(Scientist condition);
+	}
+
+	class DOMFinder : IXMLFinder {
+		
+		public List<Scientist> Find(Scientist condition) {
+			return new List<Scientist>();
+		}
+		
+	}
+	
+	class SAXFinder : IXMLFinder {
+		
+		public List<Scientist> Find(Scientist condition) {
+			return new List<Scientist>();
+		}
+		
+	}
+	
+	class LINQFinder : IXMLFinder {
+		
+		public List<Scientist> Find(Scientist condition) {
+			return new List<Scientist>();
+		}
+		
+	}
+}
---
+++ XMLParser/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace XMLParser {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow {
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void FindButton_OnClick(object sender, RoutedEventArgs e) {
+			IXMLFinder finder = GenerateFinder();
+
+			Scientist condition = new Scientist();
+			
+			List<Scientist> scientists = finder.Find(condition);
+			
+			
+		}
+
+		private IXMLFinder GenerateFinder() {
+			if (RadioDOM.IsChecked ?? false) return new DOMFinder();
+			if (RadioDOM.IsChecked ?? false) return new DOMFinder();
+			return new LINQFinder(); 
+		}
+	}
+}
