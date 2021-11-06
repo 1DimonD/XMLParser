c66523d630baf996fa3c87a1b5d2c55bfd8a1527
DimonD <dprohorchuk2002@gmail.com>
2021-11-06 20:30:17 +0200

    The frontend is fully completed. Started work on backend.

--- XMLParser/IXMLFinder.cs
+++ XMLParser/IXMLFinder.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace XMLParser {
 
 	public struct Scientist {
-		string fullName;
-		string faculty;
-		string department;
-		string scientificDegree;
-		Dictionary<string, string> academicTitles;
+		public string fullName;
+		public string faculty;
+		public string department;
+		public string scientificDegree;
+		public Dictionary<string, string> datesToAcademicTitles;
+
+		public Scientist(List<string> conditionParameters) {
+			if (conditionParameters.Count < 4) throw new Exception("Can`t create recording");
+			
+			fullName = conditionParameters[0];
+			faculty = conditionParameters[1];
+			department = conditionParameters[2];
+			scientificDegree = conditionParameters[3];
+			
+			datesToAcademicTitles = new Dictionary<string, string>();
+			var titles = conditionParameters[4].Split(',');
+			foreach(var title in titles)
+				datesToAcademicTitles.Add("Any", title.Trim());
+		}
 	}
 	
 	public interface IXMLFinder {
--- XMLParser/MainWindow.xaml.cs
+++ XMLParser/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Net.Mime;
+using System.Security.Cryptography;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -9,24 +11,69 @@ namespace XMLParser {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow {
+		private List<CheckBox> checkboxes = new List<CheckBox>();
+		private List<TextBox> textboxes = new List<TextBox>();
+		
 		public MainWindow() {
 			InitializeComponent();
+			FillConditions();
+		}
+
+		private void FillConditions() {
+			string[] options = {"ПІП", "Факультет", "Кафедра", "Науковий ступінь", "Вченні звання"};
+
+			foreach (var option in options) {
+				var stack = new StackPanel();
+				
+				var checkbox = new CheckBox();
+				checkbox.Content = option;
+				stack.Children.Add(checkbox);
+				checkboxes.Add(checkbox);
+
+				var textBox = new TextBox();
+				textBox.Height = 15;
+				textBox.Margin = new Thickness(0, 0, 0, 40);
+				stack.Children.Add(textBox);
+				textboxes.Add(textBox);
+				
+				Conditions.Children.Add(stack);
+			}
 		}
 
 		private void FindButton_OnClick(object sender, RoutedEventArgs e) {
 			IXMLFinder finder = GenerateFinder();
 
-			Scientist condition = new Scientist();
-			
-			List<Scientist> scientists = finder.Find(condition);
-			
-			
+			List<Scientist> scientists = finder.Find(GetCondition());
+
+			if (scientists.Count == 0) MessageBox.Show("Nothing were found");
+			foreach(var sc in scientists) Print(sc);
 		}
 
 		private IXMLFinder GenerateFinder() {
-			if (RadioDOM.IsChecked ?? false) return new DOMFinder();
-			if (RadioDOM.IsChecked ?? false) return new DOMFinder();
+			if (RadioDOM.IsChecked == true) return new DOMFinder();
+			if (RadioSAX.IsChecked == true) return new SAXFinder();
 			return new LINQFinder(); 
 		}
+
+		private Scientist GetCondition() {
+			List<string> parameters = new List<string>();
+			for (int i = 0; i < checkboxes.Count; i++) {
+				parameters.Add(checkboxes[i].IsChecked == true ? textboxes[i].Text : "");
+			}
+			return new Scientist(parameters);
+		}
+		
+		private void Print(Scientist sc) {
+			CurrentDocument.Text += sc.fullName + "\n";
+			CurrentDocument.Text += sc.faculty + "\n";
+			CurrentDocument.Text += sc.department + "\n";
+			CurrentDocument.Text += sc.scientificDegree + "\n";
+			foreach (var kvp in sc.datesToAcademicTitles) {
+				var date = kvp.Key;
+				var title = kvp.Value;
+				CurrentDocument.Text += title + ": " + date + "\n";
+			}
+		}
+		
 	}
 }
