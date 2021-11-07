eebb224e748e40a72e6b91ebc55f8ff78635ebb6
DimonD <dprohorchuk2002@gmail.com>
2021-11-07 16:37:07 +0200

    Added SAX solution.

--- XMLParser/IXMLFinder.cs
+++ XMLParser/IXMLFinder.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace XMLParser {
+
+	public static class File {
+		public static string filePath = @"D:\Univ_C#\Work\XMLParser\XMLParser\TestFiles\Test1.xml";
+	}
 
 	public struct Scientist {
 		public string fullName;
@@ -12,6 +17,17 @@ namespace XMLParser {
 		public string scientificDegree;
 		public Dictionary<string, string> datesToAcademicTitles;
 
+		public Scientist(string fn, string f, string d, string scD, Dictionary<string, string> dTAT) {
+			fullName = fn;
+			faculty = f;
+			department = d;
+			scientificDegree = scD;
+			datesToAcademicTitles = new Dictionary<string, string>();
+			foreach (var kvp in dTAT) {
+				datesToAcademicTitles.Add(kvp.Key, kvp.Value);
+			}
+		}
+		
 		public Scientist(List<string> conditionParameters) {
 			if (conditionParameters.Count < 4) throw new Exception("Can`t create recording");
 			
@@ -71,18 +87,84 @@ namespace XMLParser {
 		
 	}
 	
+	
 	class SAXFinder : IXMLFinder {
 		
 		public List<Scientist> Find(Scientist condition) {
-			return new List<Scientist>();
+			List<Scientist> scientists = new List<Scientist>();
+			
+			using (XmlReader xr = XmlReader.Create(File.filePath)) {
+				string fullName = "";
+				string faculty = "";
+				string department = "";
+				string scientificDegree = "";
+				string date = "";
+				string title = "";
+				var academicTitles = new Dictionary<string, string>();
+
+				string element = "";
+
+				while (xr.Read()) {
+					switch (xr.NodeType) {
+						case XmlNodeType.Element: {
+							element = xr.Name;
+							if (element == "Title") {
+								date = xr.GetAttribute("date");
+							}
+							break;
+						}
+
+						case XmlNodeType.Text: {
+							switch (element) {
+								case "FullName": fullName = xr.Value; break;
+								case "Faculty":  faculty = xr.Value; break;
+								case "Department":  department = xr.Value; break;
+								case "ScientificDegree":  scientificDegree = xr.Value; break;
+								case "Title":  title = xr.Value; break;
+							}
+
+							break;
+						}
+
+						case XmlNodeType.EndElement: {
+							if(xr.Name == "Title") {
+								if(!academicTitles.ContainsKey(date)) academicTitles.Add(date, title);
+							}
+
+							if (xr.Name == "Scientist") {
+								scientists.Add(
+									new Scientist(
+											fullName,
+											faculty,
+											department,
+											scientificDegree,
+											academicTitles
+										)
+								);
+								academicTitles.Clear();
+							}
+							
+							break;
+						}
+					}
+				}
+			}
+
+			Scientist[] copy = new Scientist[scientists.Count];
+			scientists.CopyTo(copy);
+			foreach (var sc in copy) {
+				if (sc != condition) scientists.Remove(sc);
+			}
+			return scientists;
 		}
 		
 	}
 	
+	
 	class LINQFinder : IXMLFinder {
 		
 		public List<Scientist> Find(Scientist condition) {
-			XDocument xdoc = XDocument.Load(@"D:\Univ_C#\Work\XMLParser\XMLParser\TestFiles\Test1.xml");
+			XDocument xdoc = XDocument.Load(File.filePath);
 			if (xdoc == null) throw new Exception("Can`t open the file");
 
 			var matches = from scientist in xdoc.Element("List").Elements("Scientist")
